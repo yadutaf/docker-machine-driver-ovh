78d149897ca262e5ff9faefe4222c5f13c2d1886
y8 <y8@ya.ru>
2016-08-24 00:12:51 +0200

    Package each platform in archive with own name, but keep the same driver file name.

--- bin/package.sh
+++ bin/package.sh
@@ -4,6 +4,14 @@ set -e
 
 OS="darwin linux windows"
 ARCH="amd64"
+VERSION=${TRAVIS_TAG:-latest}
+PRODUCT="docker-machine-driver-ovh"
+PKG_ROOT="pkg"
+BUILD_ROOT="build"
+
+echo "Building version $VERSION of $PRODUCT in $GOPATH"
+mkdir -p $PKG_ROOT
+mkdir -p $BUILD_ROOT
 
 echo "Getting build dependencies"
 go get
@@ -16,11 +24,23 @@ golint $pkgs
 
 for GOOS in $OS; do
     for GOARCH in $ARCH; do
-        architecture="${GOOS}-${GOARCH}"
-        echo "Building ${architecture}"
+        name="${PRODUCT}-${VERSION}-${GOOS}-${GOARCH}"
+        archive="${name}.tar.gz"
+        build_path="${BUILD_ROOT}/${name}"
+        location="${build_path}/${PRODUCT}"
+
+        echo "Building ${name}"
         export GOOS=$GOOS
         export GOARCH=$GOARCH
         go get
-        go build -o bin/docker-machine-driver-ovh-${architecture}
+        go build -o $location
+
+        echo "Packing ${location}"
+        tar -cvzf $PKG_ROOT/$archive -C $build_path $PRODUCT
     done
 done
+
+echo "Cleaning"
+rm -rf $BUILD_ROOT
+
+echo "Build completed."
